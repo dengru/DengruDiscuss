d11ee32a98c6e690340857fd58fc255f3f23e157
Dennis Grundelius <dgrundelius@gmail.com>
2016-04-27 00:03:33 +0200

    Added files via upload

---
+++ core/libs/Handler.php
@@ -0,0 +1,301 @@
+<?php
+/**
+ * @name Handler
+ * 
+ * DengruDiscuss
+ * --------------
+ * @author dennis
+ *
+ */
+
+class Handler extends DengruDiscuss {
+		
+	public $pagetitle;
+	public $sitename;
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct();
+		
+		$this->sitename = SITE_NAME;
+		
+		// Load models
+		$this->session = $this->loadModel('Session');
+		$this->user = $this->loadModel('User');	
+		$this->forum = $this->loadModel('Forum');
+		
+		// Only load the admin model if the user is authorized as an administrator
+		if ($this->session->isAdmin()) {
+			$this->admin = $this->loadModel('Admin');
+		}
+	}
+	
+	/**
+	 * Forum Index
+	 */
+	public function forumIndex() {
+		$this->pagetitle = 'Home';
+		//$this->categoryname = $this->forum->getCategoryNameById();
+
+		$this->res = $this->forum->getCategories();
+		
+		$this->render('index');
+	}
+	
+	/**
+	 * Threads
+	 */
+	public function threads() {
+		$this->categoryname = $this->forum->getCategoryName();
+		$this->categorydesc = $this->forum->getCategoryDescription();
+		$this->pagetitle = $this->categoryname;		
+		$this->res = $this->forum->getThreads();
+		
+		$this->render('threads');
+	}
+	
+	/**
+	 * Thread
+	 */
+	public function thread() {
+		$this->categoryname = $this->forum->getCategoryNameById();
+		$this->threadname = $this->forum->getThreadName();
+		$this->pagetitle = $this->threadname;	
+		$this->res = $this->forum->getThread();
+		
+		$this->render('thread');
+	}
+	
+	/**
+	 * New Thread
+	 */
+	public function newThread() {
+		if ($this->session->isLoggedIn()) {
+			$this->categoryname = $this->forum->getCategoryNameById();
+			$this->pagetitle = 'New Thread';
+			
+			$this->render('createthread');
+			
+			if (isset($_POST['create-thread'])) {
+				$this->forum->createThread();
+			}
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}
+	}
+	
+	/**
+	 * Reply To Thread
+	 */
+	public function replyToThread() {
+		if ($this->session->isLoggedIn()) {
+			$this->threadname = $this->forum->getThreadName();
+			$this->categoryname = $this->forum->getCategoryNameById();
+			
+			$this->pagetitle = 'Reply to thread: ' . $this->threadname;
+				
+			$this->render('createreply');
+				
+			if (isset($_POST['create-reply'])) {
+				$this->forum->createReply();
+			}
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}
+	}
+	
+	/**
+	 * Edit Post
+	 */
+	public function editPost() {
+		if ($this->session->isLoggedIn()) {
+			$this->threadname = $this->forum->getThreadName();
+			$this->categoryname = $this->forum->getCategoryNameById();
+			$this->postauthor = $this->forum->getPostAuthor();
+				
+			$this->pagetitle = 'Editing reply of thread: ' . $this->threadname;
+	
+			// User wants to change password? OK, lets do it
+			if (isset($_POST['update-post'])) {
+				$this->forum->updatePost();
+			}
+			
+			// Check if user is the owner to the post but also check if the user is not a admin
+			if ($this->postauthor == $_SESSION['username'] && !$this->session->isAdmin()) {
+				$this->vals = $this->forum->getPostById();
+				$this->render('updatepost');
+			// Check if user is a admin, if so then user can edit any post, not just his own
+			} else if ($this->session->isAdmin()) {
+				$this->vals = $this->forum->getPostByIdIfAdmin();
+				$this->render('updatepost');
+			}
+			else {
+				header('location: ' . CORE_URL);
+			}
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}
+	}
+	
+	
+	/**
+	 * Login Handler
+	 */
+	public function login() {
+		if ($this->session->isLoggedIn()) {
+			header('location: ' . CORE_URL);
+		}
+		else {
+			$this->pagetitle = 'Login';
+			$this->render('login');// Render the login template
+			
+			$username = isset($_POST['username']);
+			$password = isset($_POST['password']);
+				
+			if (isset($_POST['login'])) {
+				$this->session->doLoginWithPostData($username, $password);
+			}
+		}
+	}
+	
+	/**
+	 * Register Handler
+	 */
+	public function register() {
+		if (!$this->session->isLoggedIn()) {
+			$this->pagetitle = 'Register';
+			
+			// Someone wants to register, huh? Let him do it, just make sure all the inputs where populated...
+			if (isset($_POST['register']) && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['country']) && !empty($_POST['password'])) {
+				// Check if passwords match eachother, then proceed to the user creation process
+				$this->user->createNewUser();
+			}
+			
+			$this->render('register');
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}
+	}
+	
+	public function logout() {
+		if ($this->session->isLoggedIn()) {
+			$this->session->doLogout();
+			$this->render('logout');
+			header('refresh:5;url=' . CORE_URL);
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}
+	}
+	
+	/**
+	 * Account Handler
+	 */
+	public function account() {
+		
+		if ($this->session->isLoggedIn()) {
+			$this->pagetitle = 'Account Settings';
+			
+			// User wants to update account settings? Maybe change the password as well? Well, lets do it
+			if (isset($_POST['update-account']) && !empty($_POST['email']) && !empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['country'])
+			) {
+				// Check if user has entered the password fields as well, may want to change his password
+				if (!empty($_POST['password']) && !empty($_POST['password2'])) {
+						$this->user->updateUserPassword();
+				}
+				//default
+				$this->user->updateUser();
+			}
+			
+			$this->vals = $this->user->getUserDetails();
+				
+			$this->render('account');
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}
+	}
+	
+	/**
+	 * Admnistration Index
+	 */
+	public function adminIndex() {
+		$this->pagetitle = 'Adminstration';
+	
+		$this->render('admin.index');
+	}
+	
+	/**
+	 * Categories (Administration)
+	 */
+	public function adminCategories() {
+		if ($this->session->isAdmin()) {
+			$this->pagetitle = 'Categories';
+			
+			$this->res = $this->forum->getCategories();
+			
+			$this->render('admin.categories');
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}	
+	}
+	
+	/**
+	 * New Category (Administration)
+	 */
+	public function adminNewCategory() {
+		if ($this->session->isAdmin()) {
+			$this->pagetitle = 'New Category';
+				
+			if (isset($_POST['create-category']) && !empty($_POST['categoryname']) && !empty($_POST['categorydesc'])) {
+				$this->admin->createNewCategory();
+			}
+			
+			$this->render('admin.createcategory');
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}
+	}
+	
+	/**
+	 * Edit Category (Administration)
+	 */
+	public function adminEditCategory() {
+		if ($this->session->isAdmin()) {
+			$categoryId = $_GET['id'];
+			
+			$this->pagetitle = 'Edit Category';
+				
+			if (isset($_POST['update-category']) && !empty($_POST['categoryname']) && !empty($_POST['categorydesc'])) {
+				$this->admin->updateCategory();
+			}
+			
+			$this->vals = $this->admin->getCategoryDetails($categoryId);
+			$this->render('admin.editcategory');
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}
+	}
+	
+	/**
+	 * Delete Category (Administration)
+	 */
+	public function adminDeleteCategory() {	
+		if ($this->session->isAdmin()) {
+			$this->admin->removeCategory();
+		}
+		else {
+			header('location: ' . CORE_URL);
+		}
+	}
+}
+?>
---
+++ core/libs/Router.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * @name Router
+ * 
+ * DengruDiscuss
+ * --------------
+ * @author dennis
+ *
+ */
+
+class Router extends DengruDiscuss {
+	
+	public $queryString;
+	public $argString;
+	public $idString;
+	public $hostString;
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		
+		$this->queryString = 'q';
+		$this->catString = 'c';
+		$this->threadString = 't';
+		$this->argString = 'a';
+		$this->idString = 'id';
+		$this->postIdString = 'postid';
+		$this->hostString = 'HTTP_HOST';
+		
+		$this->handler = $this->loadLibrary('Handler');
+		
+		$this->run();
+	}
+	
+	
+	
+	public function run() {
+		if (!isset($_GET[$this->queryString]) && !isset($_GET[$this->argString]) && !isset($_GET[$this->catString]) && !isset($_GET[$this->threadString]) && !isset($_GET[$this->postIdString])) {
+			$this->handler->forumIndex();
+		}
+		
+		// Route requests with ?c= (category string)
+		if (!isset($_GET[$this->queryString]) && !isset($_GET[$this->argString]) && isset($_GET[$this->catString]) && !isset($_GET[$this->threadString]) && !isset($_GET[$this->postIdString])) {
+			$this->handler->threads();
+		}
+		
+		// Route requests with ?c=&t= (category string and thread string)
+		if (!isset($_GET[$this->queryString]) && !isset($_GET[$this->argString]) && isset($_GET[$this->catString]) && isset($_GET[$this->threadString]) && !isset($_GET[$this->postIdString])) {
+			$this->handler->thread();
+		}
+		
+		// Route requests with ?c=&a= (category string and argument string)
+		if (!isset($_GET[$this->queryString]) && isset($_GET[$this->argString]) && isset($_GET[$this->catString]) && !isset($_GET[$this->threadString]) && !isset($_GET[$this->postIdString])) {
+			$this->handler->newThread();
+		}
+		
+		// Route requests with ?c=&t=&a= (category string, thread string and argument string)
+		if (!isset($_GET[$this->queryString]) && isset($_GET[$this->argString]) && isset($_GET[$this->catString]) && isset($_GET[$this->threadString]) && !isset($_GET[$this->postIdString])) {
+			$this->handler->replyToThread();
+		}
+		
+		// Route requests with ?c=&t=&a=&postid (category string, thread string and argument string)
+		if (!isset($_GET[$this->queryString]) && isset($_GET[$this->argString]) && isset($_GET[$this->catString]) && isset($_GET[$this->threadString]) && isset($_GET[$this->postIdString])) {
+			$this->handler->editPost();
+		}
+		
+		//Route requests inside the administration section
+		if (isset($_GET[$this->queryString]) && isset($_GET[$this->argString]) && !isset($_GET[$this->idString]) && !isset($_GET[$this->catString]) && !isset($_GET[$this->threadString]) && !isset($_GET[$this->postIdString])) {
+			switch ($_GET[$this->queryString]) {
+				case 'admin':
+					switch ($_GET[$this->argString]) {
+						case 'categories':
+							$this->handler->adminCategories();
+							break;
+						case 'new-category':
+							$this->handler->adminNewCategory();
+							break;
+					}
+			}
+		}
+		
+		if (isset($_GET[$this->queryString]) && isset($_GET[$this->argString]) && isset($_GET[$this->idString])) {
+			switch ($_GET[$this->argString]) {
+				case 'edit-category':
+					$this->handler->adminEditCategory();
+					break;
+				case 'delete-category':
+					$this->handler->adminDeleteCategory();
+					break;
+				default:
+					$this->handler->adminIndex();
+					break;
+			}
+		}
+		
+		// Route regular requests where only the query string is used, such as ?q=account or ?q=login etc
+		if (isset($_GET[$this->queryString]) && !isset($_GET[$this->argString]) && !isset($_GET[$this->idString])) {
+			switch ($_GET[$this->queryString]) {
+				case 'login':
+					$this->handler->login();
+					break;
+				case 'register':
+					$this->handler->register();
+					break;
+				case 'logout':
+					$this->handler->logout();
+					break;
+				case 'account':
+					$this->handler->account();
+					break;
+				case 'admin':
+					$this->handler->adminIndex();
+					break;
+					
+				default:
+					$this->handler->forumIndex();
+					break;
+			}
+		}
+	}
+	
+}
+?>
